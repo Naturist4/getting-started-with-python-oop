7b34d314f7c54d3e4969e97a88bab84e7bd6134d
Naturist4 <198535938+Naturist4@users.noreply.github.com>
2025-05-15 05:22:02 +0000

    working through first two lessons.

--- start/02_03/circle.py
+++ start/02_03/circle.py
@@ -1,3 +1,20 @@
 """
 A class for representing a circle
 """
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def display_circumference(self):
+        print(f"circumference: {2 * 3.14 * self.radius} units")
+
+    def display_area(self):
+        print(f"area is: {3.14 * self.radius ** 2} square units.")
+
+
+circle1 = Circle(3)
+print(f"radius: {circle1.radius} units")
+circle1.display_circumference()
+circle1.display_area()
